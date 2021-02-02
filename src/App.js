705b8608a3101274fc4598ead3b8115cc4cb48db
kasidej02 <kasidej.kammool@gmail.com>
2021-02-02 13:35:17 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -30,6 +30,36 @@ function App() {
           </tr>
         </tbody>
       </table>
+      <br/>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Alice</td>
+            <td>male</td>
+            <td>20</td>
+          </tr>
+        </tbody>
+      </table>
+      <br/>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Kasidej Kammool 620610776</td>
+            <td>male</td>
+            <td>19</td>
+          </tr>
+        </tbody>
+      </table>
 
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
